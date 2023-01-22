2bc66e75bb013761b34c7189bc015e1781e6c2b1
unknown <imanuddjamil@gmail.com>
2023-01-23 06:30:15 +0700

    Adding DDL

---
+++ ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+password varchar(255) NOT NULL
+phone_number VARCHAR(15) NOT NULL,
+role VARCHAR(25) NOT NULL,
+created_at TIMESTAMP NOT NULL
+);
